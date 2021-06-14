739a7217c2ea7ed585d092e1e0f759d5289ed908
Claus Alberto Bienemann <64394367+donclaus0210@users.noreply.github.com>
2021-06-14 20:49:27 -0300

    Delete javascript directory

--- javascript/scroll_suave.js
+++
@@ -1,9 +0,0 @@
-$('.link-nav').on('click', function(e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top;
-
-    $('html, body').animate({
-        scrollTop: targetOffset
-    }, 500);
-});
