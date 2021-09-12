dccf16287e5c40ce6e845409d3f0a498dd899d88
Claus Alberto Bienemann <64394367+donclaus0210@users.noreply.github.com>
2021-09-12 17:03:17 -0300

    Delete javascript directory

--- javascript/changeImage.js
+++
@@ -1,8 +0,0 @@
-function openImage(event){
-    var source = event.currentTarget.getElementsByTagName("img")[0].getAttribute('src')
-    document.getElementById("img_ampliada").src = source
-    document.getElementById('tela_img_aplicada').classList = []
-}
-function closeImage(){
-    document.getElementById('tela_img_aplicada').classList = ['invisible']
-}
